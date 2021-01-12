af77d1c6aceb9b035b397d194b256acc30ede7fc
Yvonne Tang <ytang2@stanford.edu>
2021-01-11 19:06:56 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './stories/Button';
+import {Button} from "./stories/Button";
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button label={"Testing Storybook"} primary={true}/>
       </header>
     </div>
   );
